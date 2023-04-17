e2e5c46669ca771044c495bb82c6ae8f741236a0
veritedonato <veritedonato@gmail.com>
2023-04-17 14:28:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Windows4Web
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-vyb2wr)
---
+++ index.js
@@ -0,0 +1,10 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+
+function background(color) {
+  appDiv.style.backgroundColor = color;
+}
+
